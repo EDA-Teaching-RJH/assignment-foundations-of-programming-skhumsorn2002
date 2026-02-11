bae3a9437f3aa4e3adb00fe4d5df7c7711a3d9be
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-11 23:21:15 +0000

    Added menu feature

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,29 +1,45 @@
-names_list = ["Picard", "Riker", "Data", "Worf", "Troi"]
-ranks_list = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Counselor"]
-divisions_list = ["Command", "Command", "Operations", "Security", "Medical "]
-ids_list = ["01", "02", "03", "04", "05"]
+def init_database():
+    names_list = ["Picard", "Riker", "Data", "Worf", "Ducane"]
+    ranks_list = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
+    divisions_list = ["Command", "Command", "Operations", "Security", "Sciences"]
+    ids_list = ["01", "02", "03", "04", "05"]
+    return(names_list, ranks_list, divisions_list, ids_list)
+
+
+
+    
+      
+
+
+def display_menu():
+          print("\n--- MENU ---")
+          print("1. View All Crew")
+          print("2. View Crew by Diviation")
+          print("3. Search for Crew")
+          print("4. Add Member")
+          print("5. Remove Member")
+          print("6. Update Crew's Rank")
+          print("7. Calculate Payroll")
+          print("8. Crew Analysis")
+          print("0. Exit")
+          return(input("Select option: "))
+
+
+
+
+      
+      
+      
+
 
 
 def main():
-    
-    while True:
-        print("\n--- MENU ---")
-        print("1. View Crew")
-        print("2. Add Crew")
-        print("3. Remove Crew")
-        print("4. Analyze Data")
-        print("5. Exit")
+     names = init_database()[0]
+     ranks = init_database()[1]
+     divs = init_database()[2]
+     ids = init_database()[3]
 
-#def init_database():
-#def display_menu():
-#def add_member(names, ranks, divs, ids):
-#def remove_member(names, ranks, divs, ids):
-#def update_rank(names, ranks, ids):
-#def display_roster(names, ranks, divs, ids):
-#def search_crew(names, ranks, divs, ids):
-#def filter_by_division(names, divs):
-#def calculate_payroll(ranks):
-#def count_officers(ranks):
 
-#main()
 
+
+main()
---
+++ testing.py
@@ -0,0 +1,28 @@
+def init_database():
+    names_list = ["Picard", "Riker", "Data", "Worf", "Troi"]
+    ranks_list = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Counselor"]
+    divisions_list = ["Command", "Command", "Operations", "Security", "Medical "]
+    ids_list = ["01", "02", "03", "04", "05"]
+        
+    return(names_list, ranks_list, divisions_list, ids_list)
+
+def main():
+    
+def add_member(names, ranks, divs, ids):
+     #
+
+def remove_member(names, ranks, divs, ids):
+     #
+     
+def update_rank(names, ranks, ids):
+     #
+def display_roster(names, ranks, divs, ids):
+     #
+def search_crew(names, ranks, divs, ids):
+     #
+def filter_by_division(names, divs):
+     #
+def calculate_payroll(ranks):
+     #
+def count_officers(ranks):
+     #
