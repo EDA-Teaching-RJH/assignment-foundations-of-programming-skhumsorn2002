21667b555a99ad3d1a6a77019facb6cddb4ee111
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-11 23:48:38 +0000

    Add memeber

--- fleet_manager.py
+++ fleet_manager.py
@@ -2,14 +2,8 @@ def init_database():
     names_list = ["Picard", "Riker", "Data", "Worf", "Ducane"]
     ranks_list = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
     divisions_list = ["Command", "Command", "Operations", "Security", "Sciences"]
-    ids_list = ["01", "02", "03", "04", "05"]
+    ids_list = ["123", "329", "671", "621", "999"]
     return(names_list, ranks_list, divisions_list, ids_list)
-
-
-
-    
-      
-
 
 def display_menu():
           print("\n--- MENU ---")
@@ -24,21 +18,33 @@ def display_menu():
           print("0. Exit")
           return(input("Select option: "))
 
+def display_roster(names, ranks, divs, ids):
+          for i in range(len(names)):
+               print(F"{ids[i]} - {names[i]} - {ranks[i]} - {divs[i]}")
 
+def add_member(names, ranks, divs, ids):
+     add_name = input("Enter new member name: ").title()   
+     add_ranks = input("Enter new member rank: ").title()
+     add_divs = input("Enter new member divisions: ").title()
+     add_ids = input("Enter new member ID(000): ")
 
-
-      
-      
-      
-
-
+     return(names.append(add_name), ranks.append(add_ranks), divs.append(add_divs), ids.append(add_ids))
 
 def main():
      names = init_database()[0]
      ranks = init_database()[1]
      divs = init_database()[2]
      ids = init_database()[3]
+     
+     while True:
+          choices = display_menu()
+          if choices == "1":
+               print(display_roster(names, ranks, divs, ids))
 
+          elif choices == "4":
+               add_member(names, ranks, divs, ids)
+          else:
+               break
 
 
 
