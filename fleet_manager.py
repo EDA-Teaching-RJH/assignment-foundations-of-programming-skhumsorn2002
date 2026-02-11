56e03c0ad2c50c9a45651acf13ad75cd2221052e
skhumsorn2002 <sk2007@kent.ac.uk>
2026-02-11 18:36:33 +0000

    Added valueable

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+names = ["Picard", "Riker", "Data", "Worf"]
+ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+divisions = ["Command", "Command", "Operations", "Security"]
+ids = ["01", "02", "03", "04"]
+
